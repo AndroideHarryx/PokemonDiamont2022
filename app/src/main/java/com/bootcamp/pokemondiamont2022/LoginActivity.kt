ec5028fc29bbd3a1c62627bc87fec0a883572a5b
nildag <nildagomez72256@gmail.com>
2022-02-11 17:56:25 -0300

    Modificacion de login

--- app/src/main/java/com/bootcamp/pokemondiamont2022/LoginActivity.kt
+++ app/src/main/java/com/bootcamp/pokemondiamont2022/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.bootcamp.pokemondiamont2022
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -20,19 +21,29 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        var intentHost=Intent(this, PokemonHostActivity::class.java)
         btn_login = findViewById(R.id.btn_login)
         txt_registrar = findViewById(R.id.txt_registrar)
         txt_usuario = findViewById(R.id.txt_user)
         txt_contrasenia = findViewById(R.id.txt_pass)
         /*******/
-        login()
 
-    }
 
-    private fun login(){
-        btn_login.setOnClickListener(){
-
+        btn_login.setOnClickListener() {
+            if(login(txt_usuario.text.toString(), txt_contrasenia.text.toString())){
+                startActivity(intentHost)
+            }
         }
+        registrarse()
     }
 
+    private fun login(usertxt: String, passtxt: String): Boolean{
+        var esCorrecto:Boolean = false
+
+
+        return esCorrecto
+    }
+    private fun registrarse(){
+        
+    }
 }
