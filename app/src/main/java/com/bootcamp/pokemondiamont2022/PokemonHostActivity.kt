abb511f96e8831d5fbb02f028d4e1baff4762c02
Marvid21 <arguellomarcelo48@gmail.com>
2022-02-11 17:56:40 -0300

    creando vistas, principal, bienvenida y primer fragment

---
+++ app/src/main/java/com/bootcamp/pokemondiamont2022/PokemonHostActivity.kt
@@ -0,0 +1,11 @@
+package com.bootcamp.pokemondiamont2022
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PokemonHostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pokemon_host)
+    }
+}
