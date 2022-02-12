38f7ff4d2095a57db6035fbfaa4b50c502024e2b
nildag <nildagomez72256@gmail.com>
2022-02-12 20:34:43 -0300

    agregue recursos para interfaz grafica

--- app/src/main/java/com/bootcamp/pokemondiamont2022/LoginActivity.kt
+++ app/src/main/java/com/bootcamp/pokemondiamont2022/LoginActivity.kt
@@ -44,6 +44,6 @@ class LoginActivity : AppCompatActivity() {
         return esCorrecto
     }
     private fun registrarse(){
-        
+
     }
 }
--- app/src/main/java/com/bootcamp/pokemondiamont2022/MainActivity.kt
+++ app/src/main/java/com/bootcamp/pokemondiamont2022/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.bootcamp.pokemondiamont2022
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        // we used the postDelayed(Runnable, time) method
+        // to send a message with a delayed time.
+        Handler().postDelayed({
+            val intentLogin = Intent(this,LoginActivity::class.java)
+            startActivity(intentLogin)
+            finish()
+        }, 1500)
     }
 }
