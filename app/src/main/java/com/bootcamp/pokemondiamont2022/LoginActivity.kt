ca4a13640789f99562c601939bdc9b14366cd4ef
Marvid21 <arguellomarcelo48@gmail.com>
2022-02-14 13:59:18 -0300

    disenho up

--- app/src/main/java/com/bootcamp/pokemondiamont2022/LoginActivity.kt
+++ app/src/main/java/com/bootcamp/pokemondiamont2022/LoginActivity.kt
@@ -1,11 +1,22 @@
 package com.bootcamp.pokemondiamont2022
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var btn_ingresa:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_ingresa=findViewById(R.id.btn_log_inicia)
+
+        btn_ingresa.setOnClickListener{
+            startActivity(Intent(this,PokemonHostActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/bootcamp/pokemondiamont2022/PokemonHostActivity.kt
+++ app/src/main/java/com/bootcamp/pokemondiamont2022/PokemonHostActivity.kt
@@ -2,18 +2,50 @@ package com.bootcamp.pokemondiamont2022
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.viewpager.widget.ViewPager
+import com.google.android.material.navigation.NavigationView
 
 class PokemonHostActivity : AppCompatActivity() {
-    lateinit var vp_menu:ViewPager
+
+    lateinit var toggle: ActionBarDrawerToggle
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var navView: NavigationView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pokemon_host)
+        drawerLayout=findViewById(R.id.drawerLayout)
+        navView=findViewById(R.id.navView)
 
-//        vp_menu=findViewById(R.id.vp_menu)
-//
-//        val adapter = ViewPagerAdapter()
-//        vp_menu
+        toggle= ActionBarDrawerToggle(this,drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
 
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.miItem1 -> Toast.makeText(applicationContext,
+                    "Clicked Item 1",Toast.LENGTH_SHORT).show()
+                R.id.miItem2 -> Toast.makeText(applicationContext,
+                    "Clicked Item 2",Toast.LENGTH_SHORT).show()
+                R.id.miItem3 -> Toast.makeText(applicationContext,
+                    "Clicked Item 3",Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
